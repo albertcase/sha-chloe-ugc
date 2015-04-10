cb7c1ac8efff6c8d30ca8b39f1179db4156d8936
demonzhang <demon.zhang@samesamechina.com>
2015-04-10 13:56:58 +0800

    no message

--- liebiao.php
+++ liebiao.php
@@ -1,57 +1,58 @@
 <?php
-include './config/database.php';
+include_once('./config/database.php');
 include_once('./config/Pdb.php');
-include './config/Pager.class.php';
+include_once('./config/Pager.class.php');
 $db = Pdb::getDb();
-$rowcount=$db->getOne("SELECT count(*) as num FROM user_info");
+$rowcount = $db->getOne("SELECT count(*) as num FROM user_info");
 //$rowcount=$db->getOne("SELECT count(id) as num FROM trio_weiboMsg");
 
-$nowindex=1;
+$nowindex = 1;
 if(isset($_GET['page'])){
-   $nowindex=$_GET['page'];
+    $nowindex = $_GET['page'];
 }else if(isset($_GET["PB_Page_Select"])){
-	$nowindex=$_GET["PB_Page_Select"];
+	$nowindex = $_GET["PB_Page_Select"];
 }
-$page=new Pager(array("nowindex"=>$nowindex,"total"=>$rowcount,"perpage"=>30,"style"=>"page_break"));
-$sql="SELECT * FROM user_info  ORDER BY id DESC LIMIT $page->offset,30";
-$rs=$db->getAll($sql,true);
+$page = new Pager(array("nowindex" => $nowindex, "total" => $rowcount, "perpage" => 30, "style" => "page_break"));
+$sql = "SELECT * FROM user_info  ORDER BY id DESC LIMIT $page->offset,30";
+$rs = $db->getAll($sql,true);
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <div align="center">
+
+<table>
+<tr>
+<td align='left'><?php echo $rowcount;?>条数据</td>
+<td><?php echo $page->show(2);?></td>
+</tr>
+</table>
+</div>
+<form action='' method='post'>
+<table border='1' width='100%'>
+<tr>
+<td width='50' align='center'>id</td>
+<td width='50' align='center'>photo</td>
+<td width='100' align='center'>name</td>
+<td width='80' align='center'>function</td>
+<td width='300' align='center'>location</td>
+<td width='50' align='center'>email</td>
+<td width='50' align='center'>createtime</td>
+</tr>
 <?php
-echo "<table><tr>";
-echo "<td align='left'>".$rowcount.'条数据</td><td>';
-echo $page->show(2)."</td></tr></table>";
-echo "</div>";
-echo "<form action='' method='post'>";
-echo "<table border='1' width='100%'>";
-echo "<tr>";
-echo "<td width='50' align='center'>id</td>";
-echo "<td width='50' align='center'>photo</td>";
-echo "<td width='100' align='center'>name</td>";
-echo "<td width='80' align='center'>function</td>";
-echo "<td width='300' align='center'>location</td>";
-echo "<td width='50' align='center'>email</td>";
-echo "<td width='50' align='center'>createtime</td>";
-echo "</tr>";
-for($i=0;$i<count($rs);$i++)
-{
-	?>
-		   <tr>
-	          <td align='center'><?php echo $rs[$i]['id']; ?></td>
-	          <td align='center'><img height="200px" src="<?php echo $rs[$i]['photo']; ?>"></td>
-	          <td align='center'><?php echo $rs[$i]['name']; ?></td>
-	          <td align='center'><?php echo $rs[$i]['function']; ?></td>
-	          <td align='center'><?php echo $rs[$i]['location']; ?></td>
-	          <td align='center'><?php echo $rs[$i]['email']; ?></td>
-	          <td align='center'><?php echo $rs[$i]['createtime']; ?></td>
-		  </tr>
-		
-	
-		
+	for($i = 0; $i < count($rs); $i++)
+	{
+?>
+	   <tr>
+          <td align='center'><?php echo $rs[$i]['id']; ?></td>
+          <td align='center'><img height="200px" src="<?php echo $rs[$i]['photo']; ?>"></td>
+          <td align='center'><?php echo $rs[$i]['name']; ?></td>
+          <td align='center'><?php echo $rs[$i]['function']; ?></td>
+          <td align='center'><?php echo $rs[$i]['location']; ?></td>
+          <td align='center'><?php echo $rs[$i]['email']; ?></td>
+          <td align='center'><?php echo $rs[$i]['createtime']; ?></td>
+	   </tr>	
 <?php
 	    
-}
-	echo  "</table></form>";
-    
+	}
 ?>
+</table></form>
+    
