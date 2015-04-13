957f767733d7dcc960fa25af13d0fd3e05891368
Spring.Zhou <spring.zhou@samesamechina.com>
2015-04-13 11:48:50 +0800

    login.js

--- js/login.js
+++ js/login.js
@@ -41,80 +41,8 @@ function errorPrompt(success){
 	
 }
 
-//lightSpeedOut
-$(".changelink").click(function(){
-	if(document.myform.name.value==""){
-		document.myform.name.focus();
-		errorPrompt();
-		$("#errorPrompt").html("请输入用户名!");
-		testAnim("#animationSandbox_login","shake","","");
-		return false;
-	}
-	testAnim("#animationSandbox_login","flipOutY","#animationSandbox_login","#animationSandbox_changepw");
-	changeFun(document.myform.name.value);
-})
 
-$(".reback").click(function(){
-	$("#newspassword").val("");
-	$("#oldpassword").val("");
-	testAnim("#animationSandbox_changepw","flipOutY","#animationSandbox_changepw","#animationSandbox_login");
-})
 
-/*$(".help").hover(function(){
-	$("#helpTips").show();
-	testAnim("#helpTips","bounceInUp","","");
-},function(){
-	$("#helpTips").hide();
-	testAnim("#helpTips","bounceOutUp","","");
-})
-*/
-
-function changeFun(loginUserName,newPassword){
-	$.get("../password.php",{"LoginName": loginUserName,"newPwd":newPassword},function(result){
-		//console.log(document.myform.name.value);
-		if(!newPassword){
-			var resultVal = JSON.parse(result);
-			/*if(resultVal.Member[0].Password=="00000000"){
-				$(".forbidden").show();
-				return false;
-			}else{*/
-				
-				if(resultVal.Member.length==0){
-					errorPrompt();
-					$("#errorPrompt").html("没有你这个人！or 长的系统都不识别！");
-					setTimeout('$(".reback").click();',1500)
-					//return false;
-				}else{
-					$("#oldpassword").val(document.myform.password.value);
-				}	
-				$("#oldpassword").attr("data-num",resultVal.Member[0].Password);
-				
-			//}
-		}else{
-			errorPrompt("success");
-			$("#errorPrompt").html("你给我记着！");	
-			setTimeout('$(".reback").click();',1500)
-		}
-		
-	})
-}
-
-$(".modify_btn").click(function(){
-	var newsVal = $("#newspassword").val();
-	var oldVal = $("#oldpassword").val();
-	var dataNum = $("#oldpassword").attr("data-num");
- 	if(newsVal==""||newsVal.length<6){
-		errorPrompt();
-		$("#errorPrompt").html("这么屌的密码至少有6位！");	
-		testAnim("#animationSandbox_changepw","shake","","");
-	}else if(oldVal!=dataNum){
-		errorPrompt();
-		$("#errorPrompt").html("不对！什么？又忘了？！去跪舔IT吧");	
-		testAnim("#animationSandbox_changepw","shake","","");
-	}else{
-	    changeFun(document.myform.name.value,newsVal);	
-	}
-})
 
 
 
